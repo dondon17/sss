4d83d4ed0e2430e2f5c268d8b5b36dd92090e749
dondon17 <qwerqw889@ajou.ac.kr>
2020-03-31 13:04:25 +0900

    stat함수 사용

--- ex24.c
+++ ex24.c
@@ -15,13 +15,13 @@ int main(void){
     printf("Nlink = %o\n", (unsigned int)buf.st_nlink); // 하드링크의 개수
     printf("UID = %d\n", (int)buf.st_uid); // 파일 소유자의 uid
     printf("GID = %d\n", (int)buf.st_gid); // 파일 소유 그룹의 gid
-    printf("SIZE = %d\n", (int)buf.st_size); 
+    printf("SIZE = %d\n", (int)buf.st_size); // 파일 내용의 크기
     printf("Atime = %d\n", (int)buf.st_atime); // 마지막으로 파일을 읽거나, 실행한 시각
     printf("Mtime = %d\n", (int)buf.st_mtime); // 마지막으로 파일의 내용을 변경한 시각
     printf("Ctime = %d\n", (int)buf.st_ctime); // 마지막으로 inode의 내용을 변경한 시각
     printf("Blksize = %d\n", (int)buf.st_blksize); // 파일의 내용을 입출력할 때 사용하는 버퍼의 크기
     printf("Blocks = %d\n", (int)buf.st_blocks); // 파일을 512bytes 씩 블록으로 나눈 개수
     //printf("FStype = %s\n", buf.st_fstype);
-
+    // 이유는 모르겠으나 stat 구조체에 st_fstpye이 정의되어 있지 않다고 한다.
     return 0;
 }
