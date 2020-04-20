b4892b84805160aa9dc338fe6baee8775c869eb9
dondon17 <qwerqw889@ajou.ac.kr>
2020-04-20 20:56:04 +0900

    상수를 이용해 파일의 종류, 접근 권한 검색 : st_mode와 &연산 수행

--- examples/ch03/stat_const.c
+++ examples/ch03/stat_const.c
@@ -24,6 +24,9 @@ int main(int argc, char **argv){
             break;
     }
 
+    if((buf.st_mode & __S_IREAD) != 0) printf("unix.txt : user has read permission\n");
+    if((buf.st_mode &(__S_IREAD >> 3)) != 0) printf("unix.txt : group has read permission\n");
+    if((buf.st_mode & S_IROTH) != 0)printf("unix.txt : other has read permission\n");
     return 0;
 }
 
@@ -34,3 +37,6 @@ int main(int argc, char **argv){
 // 파일명으로 파일의 정보를 검색하는 함수 stat(char *filename, struct stat *buf)
 // 매개변수로 입력받은 파일에 대한 정보를 stat구조체 변수에 저장
 // 파일의 정보에는 inode 번호, 파일의 종류, uid, gid, 링크 수, 접근권한 등 다양한 정보가 존재한다.
+
+// st_mode값에 접근 권한 상수를 &연산시에는 해당 권한을 가지고 있는지 확인할 수 있다.
+// 또한 S_IREAD는 사용자의 읽기 권한인데 접근 권한 비트에서 3비트 오른쪽으로 이동시키면 그룹의 읽기 권한임을 알 수 있다.
