7847c5e3b475cfa1c405ad5270df9c4db08b5d5e
dondon17 <qwerqw889@ajou.ac.kr>
2020-04-24 11:56:01 +0900

    as1

---
+++ as1/prog1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+
+int main(void){
+    FILE *fp;
+    int id, s1, s2, s3, n;
+
+    if((fp = fopen("data", "r")) == NULL){
+        perror("fopen: data");
+        exit(1);
+    }
+    
+    printf("   학번   평균   \n");
+    while((n = fscanf(fp, "%d %d %d %d", &id, &s1, &s2, &s3)) != EOF)
+        printf("%d : %.2lf\n", id, (s1+s2+s3)/3.0);
+    
+    fclose(fp);
+    return 0;
+}
